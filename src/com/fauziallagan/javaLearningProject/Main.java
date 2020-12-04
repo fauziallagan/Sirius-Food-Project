af9d9207e8052af466c5b3b80eb47ea647e3aea7
fauziallagan <fauziallagan@gmail.com>
2020-12-04 17:15:38 +0700

    new Commit

---
+++ src/com/fauziallagan/javaLearningProject/Main.java
@@ -0,0 +1,62 @@
+package com.fauziallagan.javaLearningProject;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        Scanner inputUser = new Scanner(System.in);
+        String userOption;
+        boolean isNext = true;
+        while (isNext){
+            clearScreen();
+            System.out.println("===================");
+            System.out.println("Sirius Food Project");
+            System.out.println("===================");
+            System.out.println("1. \tMakanan");
+            System.out.println("2. \tMinuman");
+            System.out.println("3. \tdessert");
+
+            System.out.print("\n\nPilihan Anda : ");
+            userOption = inputUser.next();
+
+            switch (userOption) {
+                case "1" -> System.out.println("MAKANAN");
+                case "2" -> System.out.println("MINUMAN");
+                case "3" -> System.out.println("DESSERT");
+                default -> System.err.println("\n404 Not Found! Try Again!");
+            }
+
+
+            isNext = getChoice("Apakah Anda Ingin Melanjutkan ");
+
+        }
+
+    }
+    public static boolean getChoice(String message){
+        Scanner inputUser = new Scanner(System.in);
+        System.out.print("\n" + message + "(y/n) ? ");
+        String userOption = inputUser.next();
+        while (!userOption.equalsIgnoreCase("y") && !userOption.equalsIgnoreCase("n")){
+            System.err.println("Wrong Choice !. Try Again");
+            System.out.print("\n" + message + "(y/n) ? ");
+            userOption  = inputUser.next();
+        }
+        return userOption.equalsIgnoreCase("y");
+
+    }
+    public static void clearScreen(){
+        try{
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            }else {
+                System.out.print("\n033\143");
+            }
+        }catch (Exception ex){
+            System.err.println("Not Supported!");
+        }
+    }
+
+}
